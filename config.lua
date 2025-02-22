8924a854f928e5ede1dd6b69df81fbb2984f8342
JanSharp <steinmeister36@gmail.com>
2025-02-22 01:11:06 +0100

    Improve linux docs for config.lua

--- config.lua
+++ config.lua
@@ -1,7 +1,7 @@
 
 return {
   -- On Linux you can follow this: https://github.com/lay295/TwitchDownloader?tab=readme-ov-file#linux--getting-started
-  --   Though if you're on Arch you can also just run 'yay -S twitch-downloader-bin' and keep "TwitchDownloaderCLI", since it'll be in PATH
+  --   Though if you're on Arch you can also just run 'yay -S twitch-downloader-bin' and leave the setting here unchanged, since it'll be in PATH
   --   Otherwise use: "/directory/containing/TwitchDownloaderCLI/TwitchDownloaderCLI"
   --
   -- On Windows you'd follow these steps, except for step 5: https://github.com/lay295/TwitchDownloader?tab=readme-ov-file#windows---getting-started
